85bbaa8e180b3e8771847007803aeb04541985b5
Claudio Torcato <claudiotorcato@gmail.com>
2023-06-18 01:32:00 -0300

    tema sobrio como padrão

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "sobrio",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
