a6975547f54158a99efd0409bca9e6721ce65c2c
Claudio Torcato <claudiotorcato@gmail.com>
2023-06-29 01:23:52 -0300

    keybind para fechar tab

--- mappings.lua
+++ mappings.lua
@@ -9,6 +9,7 @@ return {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+    ["<leader>bt"] = { "<cmd>tabc<cr>", desc = "Close tab" },
     ["<leader>bD"] = {
       function()
         require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
